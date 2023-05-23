906d156029df43be35d73a349bee500f728061f5
Robert Kenny <kenoir@gmail.com>
2023-05-23 17:34:01 +0100

    Add API Gateway S3 integration

--- packages/cdk/lib/pressreader.ts
+++ packages/cdk/lib/pressreader.ts
@@ -4,26 +4,95 @@ import { GuS3Bucket } from '@guardian/cdk/lib/constructs/s3';
 import { GuScheduledLambda } from '@guardian/cdk/lib/patterns/scheduled-lambda';
 import type { App } from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
+import { AwsIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Schedule } from 'aws-cdk-lib/aws-events';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import {
+	Effect,
+	PolicyStatement,
+	Role,
+	ServicePrincipal,
+} from 'aws-cdk-lib/aws-iam';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { Size } from 'aws-cdk-lib/core';
 
 export class PressReader extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 		const pressReaderApp = 'pressreader';
 
+		// S3 Bucket
 		const dataBucket = new GuS3Bucket(this, 'PressreaderDataBucket', {
 			app: pressReaderApp,
 			bucketName: `gu-pressreader-data-${this.stage.toLowerCase()}`,
 		});
 
+		// API Gateway
+		const apiGateway = new RestApi(this, 'PressReaderAPI', {
+			restApiName: 'Press Reader API',
+			description: 'Serves data to Press Reader from an S3 bucket.',
+			binaryMediaTypes: ['application/json'],
+			minCompressionSize: Size.bytes(0),
+		});
+
+		const executeRole = new Role(this, 'ApiGatewayS3AssumeRole', {
+			assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+			roleName: 'APIGatewayS3IntegrationRole',
+		});
+
+		executeRole.addToPolicy(
+			new PolicyStatement({
+				resources: [dataBucket.bucketArn],
+				actions: ['s3:Get'],
+			}),
+		);
+
+		const s3Integration = new AwsIntegration({
+			service: 's3',
+			integrationHttpMethod: 'GET',
+			path: `${dataBucket.bucketName}/{key}`,
+			options: {
+				credentialsRole: executeRole,
+				integrationResponses: [
+					{
+						statusCode: '200',
+						responseParameters: {
+							'method.response.header.Content-Type':
+								'integration.response.header.Content-Type',
+						},
+					},
+				],
+				requestParameters: {
+					'integration.request.path.key': 'method.request.path.key',
+				},
+			},
+		});
+
+		apiGateway.root
+			.addResource('data')
+			.addResource('{key}')
+			.addMethod('GET', s3Integration, {
+				methodResponses: [
+					{
+						statusCode: '200',
+						responseParameters: {
+							'method.response.header.Content-Type': true,
+						},
+					},
+				],
+				requestParameters: {
+					'method.request.path.key': true,
+					'method.request.header.Content-Type': true,
+				},
+			});
+
+		// Secret
 		const capiSecret = new Secret(this, 'CapiTokenSecret', {
 			secretName: `/${this.stage}/${this.stack}/${pressReaderApp}/capiToken`,
 			description: 'The CAPI token used to retrieve content',
 		});
 
+		// Scheduled Lambda
 		const capiSecretGetPolicyStatement = new PolicyStatement({
 			effect: Effect.ALLOW,
 			actions: ['secretsmanager:GetSecretValue'],
