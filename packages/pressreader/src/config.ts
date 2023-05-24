e536a288f0df8fca5281160b228a39784d8bcda3
Pete F <pete.faulconbridge@theguardian.com>
2023-05-24 16:40:59 +0100

    Update config fixture

--- packages/pressreader/src/config.ts
+++ packages/pressreader/src/config.ts
@@ -1,4 +1,4 @@
-import type { PressReaderEditionConfig } from './types/PressReaderConfigTypes';
+import type { PressReaderEditionConfig } from './types/PressReaderTypes';
 
 export const editionConfig: PressReaderEditionConfig = {
 	sections: [
@@ -15,5 +15,43 @@ export const editionConfig: PressReaderEditionConfig = {
 			],
 			capiSources: [],
 		},
+		{
+			displayName: 'News',
+			maximumArticleCount: 14,
+			frontSources: [
+				{
+					collectionIndexes: [1],
+					collectionNames: ['Australia news'],
+					sectionContentURL:
+						'http://api.nextgen.guardianapps.co.uk/australia-news/lite.json',
+				},
+				{
+					collectionIndexes: [],
+					collectionNames: ['Across the country'],
+					sectionContentURL:
+						'http://api.nextgen.guardianapps.co.uk/au/lite.json',
+				},
+			],
+			capiSources: [],
+		},
+		{
+			displayName: 'Politics',
+			maximumArticleCount: 8,
+			frontSources: [
+				{
+					collectionIndexes: [],
+					collectionNames: ['Australian politics'],
+					sectionContentURL:
+						'http://api.nextgen.guardianapps.co.uk/australia-news/lite.json',
+				},
+			],
+			capiSources: [
+				'search?tag=australia-news%2Faustralian-politics&order-by=newest',
+			],
+		},
+	],
+	bannedTags: [
+		'sport/series/talking-horses',
+		'science/series/alex-bellos-monday-puzzle',
 	],
 };
