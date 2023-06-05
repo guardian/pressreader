821f905d5ba55e319410805ba34bfe9ebd1a73df
Pete F <pete.faulconbridge@theguardian.com>
2023-06-05 16:43:54 +0100

    Add prefix from ENV to beginning of S3 object path

--- packages/pressreader/src/constants.ts
+++ packages/pressreader/src/constants.ts
@@ -1,5 +1,6 @@
 export const awsRegion = process.env.AWS_REGION ?? 'eu-west-1';
 export const bucketName = process.env.BUCKET_NAME ?? 'dev-pressreader';
+export const prefixPath = process.env.PREFIX_PATH ?? '';
 export const editionKey = process.env.EDITION_KEY;
 export const capiSecretLocation =
 	process.env.CAPI_SECRET_LOCATION ??
