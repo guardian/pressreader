072b47172138a09484192195d1272e789085b2d4
Robert Kenny <kenoir@gmail.com>
2023-06-14 10:29:49 +0100

    Reduce lambda alarm threshold
    This change will cause an alarm state to be set as soon
    as the lambda errors (within the first minute), rather than
    waiting for the 2 hour period to elapse.

--- packages/cdk/lib/pressreader.ts
+++ packages/cdk/lib/pressreader.ts
@@ -191,8 +191,8 @@ export class PressReader extends GuStack {
 				alarmDescription: `Triggers if there are errors from ${appName} on ${this.stage}`,
 				snsTopicName: alarmSnsTopic.topicName,
 				toleratedErrorPercentage: 1,
-				// Requires 2 failures in a row based on lambda scheduled to run once an hour
-				numberOfMinutesAboveThresholdBeforeAlarm: 120,
+				// Notify immediately if any failure occurs
+				numberOfMinutesAboveThresholdBeforeAlarm: 1,
 			};
 
 			const s3PutPolicyStatement = new PolicyStatement({
