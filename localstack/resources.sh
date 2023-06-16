01ee81f59660eace46238f3640db688473e11f78
Robert Kenny <kenoir@gmail.com>
2023-06-16 09:21:52 +0100

    pull dev secret from secretsmanager

--- localstack/resources.sh
+++ localstack/resources.sh
@@ -2,13 +2,20 @@
 
 export AWS_DEFAULT_REGION=eu-west-1
 
+# Set CAPI_API_KEY from real AWS values
+CAPI_API_KEY=$(AWS_REGION=eu-west-1 AWS_PROFILE=printProd \
+  aws secretsmanager get-secret-value \
+  --secret-id /DEV/print-production/pressreader/capiToken \
+  --query "SecretString" \
+  --output text)
+
 awslocal secretsmanager delete-secret \
   --secret-id "/DEV/print-production/pressreader/capiToken" \
   --force-delete-without-recovery || true
 
 awslocal secretsmanager create-secret \
   --name /DEV/print-production/pressreader/capiToken \
-  --secret-string changeme 
+  --secret-string $CAPI_API_KEY 
 
 # Create our telemetry bucket for localstack
 awslocal s3 mb s3://dev-pressreader  || true
