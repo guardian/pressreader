f8fad2fb8ba39f4a138a796fc56343701aa2003d
Pete F <pete.faulconbridge@theguardian.com>
2023-08-23 11:59:43 +0100

    Run the 'new' lambdas less frequently

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -9,10 +9,14 @@ const app = new GuRootExperimental();
  * Each of the INFRA lambdas is scheduled to run every 15 minutes, but the schedules
  * are offset by 2 minutes to avoid all lambdas running at the same time, to reduce
  * the likelihood of hitting the CAPI rate limit.
+ * The monitoring for each lambda assumes that they're running every 15 minutes, so
+ * the 'new' lambdas (which are not yet used in production) are schedules to run
+ * every 4 times in one hour per day. This way we should be alerted to any issues,
+ * without running them too often.
  */
 const infraSchedules = [
-	Schedule.cron({ minute: '0,15,30,45' }),
-	Schedule.cron({ minute: '2,17,32,47' }),
+	Schedule.cron({ minute: '0,15,30,45', hour: '10' }),
+	Schedule.cron({ minute: '2,17,32,47', hour: '10' }),
 	Schedule.cron({ minute: '4,19,34,49' }),
 	Schedule.cron({ minute: '6,21,36,51' }),
 ] as const;
@@ -34,12 +38,22 @@ new PressReader(app, 'PressReader-INFRA', {
 			schedule: infraSchedules[1],
 		},
 		{
+			/**
+			 * This is the 'legacy' AUS config, which writes to the bucket currently
+			 * used in production. It should be removed after Pressreader start using
+			 * the new API key.
+			 * */
 			editionKey: 'AUS',
 			s3PrefixPath: [],
 			bucketName: 'press-reader-aus-configs',
 			schedule: infraSchedules[2],
 		},
 		{
+			/**
+			 * This is the 'legacy' US config, which writes to the bucket currently
+			 * used in production. It should be removed after Pressreader start using
+			 * the new API key.
+			 * */
 			editionKey: 'US',
 			s3PrefixPath: [],
 			bucketName: 'press-reader-us-configs',
@@ -49,11 +63,6 @@ new PressReader(app, 'PressReader-INFRA', {
 	domainName: 'pressreader.gutools.co.uk',
 });
 
-/**
- * Each of the INFRA lambdas is scheduled to run once a day, but the schedules
- * are offset by 2 minutes to avoid all lambdas running at the same time, to reduce
- * the likelihood of hitting the CAPI rate limit.
- */
 const codeSchedules = [
 	Schedule.cron({ minute: '0', hour: '10' }),
 	Schedule.cron({ minute: '2', hour: '10' }),
