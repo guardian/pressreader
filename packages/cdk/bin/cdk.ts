bebc9920f9d0bba1d3033d8242958809dde01f0c
Pete F <pete.faulconbridge@theguardian.com>
2023-09-15 10:24:21 +0100

    Remove legacy lambdas

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -9,16 +9,10 @@ const app = new GuRootExperimental();
  * Each of the INFRA lambdas is scheduled to run every 15 minutes, but the schedules
  * are offset by 2 minutes to avoid all lambdas running at the same time, to reduce
  * the likelihood of hitting the CAPI rate limit.
- * The monitoring for each lambda assumes that they're running every 15 minutes, so
- * the 'new' lambdas (which are not yet used in production) are schedules to run
- * every 4 times in one hour per day. This way we should be alerted to any issues,
- * without running them too often.
  */
 const infraSchedules = [
 	Schedule.cron({ minute: '0,15,30,45' }),
 	Schedule.cron({ minute: '2,17,32,47' }),
-	Schedule.cron({ minute: '4,19,34,49' }),
-	Schedule.cron({ minute: '6,21,36,51' }),
 ] as const;
 
 new PressReader(app, 'PressReader-INFRA', {
@@ -37,28 +31,6 @@ new PressReader(app, 'PressReader-INFRA', {
 			s3PrefixPath: ['data', 'US'],
 			schedule: infraSchedules[1],
 		},
-		{
-			/**
-			 * This is the 'legacy' AUS config, which writes to the bucket currently
-			 * used in production. It should be removed after Pressreader start using
-			 * the new API key.
-			 * */
-			editionKey: 'AUS',
-			s3PrefixPath: [],
-			bucketName: 'press-reader-aus-configs',
-			schedule: infraSchedules[2],
-		},
-		{
-			/**
-			 * This is the 'legacy' US config, which writes to the bucket currently
-			 * used in production. It should be removed after Pressreader start using
-			 * the new API key.
-			 * */
-			editionKey: 'US',
-			s3PrefixPath: [],
-			bucketName: 'press-reader-us-configs',
-			schedule: infraSchedules[3],
-		},
 	],
 	domainName: 'pressreader.gutools.co.uk',
 });
@@ -66,8 +38,6 @@ new PressReader(app, 'PressReader-INFRA', {
 const codeSchedules = [
 	Schedule.cron({ minute: '0', hour: '10' }),
 	Schedule.cron({ minute: '2', hour: '10' }),
-	Schedule.cron({ minute: '4', hour: '10' }),
-	Schedule.cron({ minute: '6', hour: '10' }),
 ] as const;
 
 new PressReader(app, 'PressReader-CODE', {
@@ -85,18 +55,6 @@ new PressReader(app, 'PressReader-CODE', {
 			editionKey: 'US',
 			s3PrefixPath: ['data', 'US'],
 			schedule: codeSchedules[1],
-		},
-		{
-			editionKey: 'AUS',
-			s3PrefixPath: ['old-data', 'AUS'],
-			bucketName: 'gu-pressreader-data-code',
-			schedule: codeSchedules[2],
-		},
-		{
-			editionKey: 'US',
-			s3PrefixPath: ['old-data', 'US'],
-			bucketName: 'gu-pressreader-data-code',
-			schedule: codeSchedules[3],
 		},
 	],
 	domainName: 'pressreader.code.dev-gutools.co.uk',
